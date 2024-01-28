fb8588c29977297772c5008f1cd805a081559c56
gudtldn <gudtldn1234@gmail.com>
2024-01-29 01:24:00 +0900

    로깅 레벨 변경

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import os
 import dotenv
+import logging
 
 from classes.bot import Bot
 
 dotenv.load_dotenv() # .env 파일 로드
 
-    
+
 bot = Bot()
 
 
 
-bot.run(os.environ.get("DISCORD_BOT_TOKEN"))
+bot.run(os.environ.get("DISCORD_BOT_TOKEN"), log_level=logging.DEBUG)
