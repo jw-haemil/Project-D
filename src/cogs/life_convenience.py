b1d343d119f1ae92c0a84115c45f417e92eae398
gudtldn <gudtldn1234@gmail.com>
2024-01-28 13:21:06 +0900

    파일명, 클래스명 변경

--- src/cogs/life_convenience.py
+++ src/cogs/life_convenience.py
@@ -4,11 +4,11 @@ from discord.ext import commands
 from classes.bot import Bot
 
 
-class Life(commands.Cog):
+class LifeConvenience(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
 
 
 async def setup(bot: Bot): # setup 함수로 명령어 추가
-    await bot.add_cog(Life(bot))
+    await bot.add_cog(LifeConvenience(bot))
