43cabe4600e5a79e12c98c27b872d81506972d7b
gudtldn <gudtldn1234@gmail.com>
2024-01-29 19:25:41 +0900

    로그파일 코드 추가

--- src/main.py
+++ src/main.py
@@ -2,9 +2,27 @@ import os
 import dotenv
 import logging
 
+from datetime import datetime
+
 from classes.bot import Bot
 
 dotenv.load_dotenv() # .env 파일 로드
+
+
+# 로그 파일 설정
+handler = logging.FileHandler(
+    filename=f"./logs/{datetime.today().strftime('%Y-%m-%d_%H-%M-%S')}.log",
+    encoding="utf-8",
+    mode="w"
+)
+handler.setFormatter(
+    logging.Formatter(
+        fmt="[{asctime}] [{levelname:<8}] {name}: {message}",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        style="{"
+    )
+)
+logging.getLogger().addHandler(handler)
 
 
 bot = Bot()
