a9230ed92c20610cb4e5016044ece3446d493f01
gudtldn <gudtldn1234@gmail.com>
2024-01-31 14:32:37 +0900

    send를 reply로 변경

--- src/cogs/finance.py
+++ src/cogs/finance.py
@@ -21,13 +21,13 @@ class Finance(Cog):
             user = ctx.author
             user_info = self.bot.database.get_user_info(user.id)
             if not await user_info.is_valid_user():
-                await ctx.send("사용자 등록을 먼저 해 주세요.")
+                await ctx.reply("사용자 등록을 먼저 해 주세요.")
                 return
         else:
             user = other_user
             user_info = self.bot.database.get_user_info(user.id)
             if not await user_info.is_valid_user():
-                await ctx.send(f"{other_user.display_name}님은 등록되어 있지 않은 유저입니다.")
+                await ctx.reply(f"{other_user.display_name}님은 등록되어 있지 않은 유저입니다.")
                 return
 
         embed = discord.Embed(
@@ -35,7 +35,7 @@ class Finance(Cog):
             color=0x00ff00
         )
         embed.add_field(name="자산", value=f"{await user_info.get_money():,}원")
-        await ctx.send(embed=embed)
+        await ctx.reply(embed=embed)
 
 
     @commands.command(
@@ -48,17 +48,17 @@ class Finance(Cog):
         user_info = self.bot.database.get_user_info(ctx.author.id)
 
         if not await user_info.is_valid_user(): # 사용자 등록 여부 확인
-            await ctx.send("사용자 등록을 먼저 해 주세요.")
+            await ctx.reply("사용자 등록을 먼저 해 주세요.")
             return
 
         check_time = datetime.utcfromtimestamp(await user_info.get_check_time()) # 출석체크 시간 가져오기
         if check_time.date() + timedelta(days=1) <= datetime.utcnow().date(): # 시간 비교
             if random.randint(0, 999) == 511:
                 money = 5000
-                await ctx.send(f"출석체크 완료! 축하합니다! 0.1% 확률을 뚫고 5,000원을 받았습니다.")
+                await ctx.reply(f"출석체크 완료! 축하합니다! 0.1% 확률을 뚫고 5,000원을 받았습니다.")
             else:
                 money = random.randint(1, 10) * 100
-                await ctx.send(f"출석체크 완료! {money:,}원을 받았습니다.")
+                await ctx.reply(f"출석체크 완료! {money:,}원을 받았습니다.")
 
             await user_info.add_money(money) # 돈 추가
             await user_info.set_check_time(int(time.time())) # 출석체크 시간 업데이트
@@ -78,37 +78,37 @@ class Finance(Cog):
         other_user_info = self.bot.database.get_user_info(other_user.id)
 
         if not await user_info.is_valid_user(): # 사용자 등록 여부 확인
-            await ctx.send("사용자 등록을 먼저 해 주세요.")
+            await ctx.reply("사용자 등록을 먼저 해 주세요.")
             return
         elif not await other_user_info.is_valid_user(): # 사용자 등록 여부 확인
-            await ctx.send(f"{other_user.display_name}님은 등록되어 있지 않은 유저입니다.")
+            await ctx.reply(f"{other_user.display_name}님은 등록되어 있지 않은 유저입니다.")
             return
         elif other_user.id == ctx.author.id: # 자기 자신에게 돈을 송금하는 경우 예외처리
-            await ctx.send("자기 자신에게 돈을 송금할 수 없습니다.")
+            await ctx.reply("자기 자신에게 돈을 송금할 수 없습니다.")
             return
         elif money <= 0:
-            await ctx.send("송금 금액은 1원 이상이어야 합니다.")
+            await ctx.reply("송금 금액은 1원 이상이어야 합니다.")
             return
 
         user_money = await user_info.get_money() # 돈 정보를 가져옴
         if money > user_money: # 송금이 불가능하면
-            await ctx.send("돈이 부족합니다.")
+            await ctx.reply("돈이 부족합니다.")
             return
 
         await user_info.set_money(user_money - money)
         await other_user_info.add_money(money)
-        await ctx.send(f"{other_user.display_name}님에게 {money:,}원을 송금했습니다.")
+        await ctx.reply(f"{other_user.display_name}님에게 {money:,}원을 송금했습니다.")
 
     @send_money.error
     async def send_money_error(self, ctx: commands.Context, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("보낼 사람과 돈을 입력해 주세요.")
+            await ctx.reply("보낼 사람과 돈을 입력해 주세요.")
         elif isinstance(error, commands.UserNotFound):
-            await ctx.send("보낼 사람을 찾을 수 없습니다.")
+            await ctx.reply("보낼 사람을 찾을 수 없습니다.")
         elif isinstance(error, commands.BadArgument):
-            await ctx.send("보낼 사람과 돈은 정수로 입력해 주세요.")
+            await ctx.reply("보낼 사람과 돈은 정수로 입력해 주세요.")
         else:
-            await ctx.send("오류가 발생했습니다.")
+            await ctx.reply("오류가 발생했습니다.")
             raise error
 
 
--- src/cogs/user_management.py
+++ src/cogs/user_management.py
@@ -15,11 +15,11 @@ class UserManagement(Cog):
         user_info = self.bot.database.get_user_info(ctx.author.id)
 
         if await user_info.is_valid_user():
-            await ctx.send("이미 등록된 사용자입니다.")
+            await ctx.reply("이미 등록된 사용자입니다.")
             return
 
         await user_info.add_user()
-        await ctx.send("사용자를 등록했습니다.")
+        await ctx.reply("사용자를 등록했습니다.")
 
 
 async def setup(bot: Bot): # setup 함수로 명령어 추가
