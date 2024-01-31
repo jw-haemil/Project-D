869e3ed991d476b881f87a69351f285d74cb6da8
gudtldn <gudtldn1234@gmail.com>
2024-01-31 12:01:44 +0900

    코드 최적화

--- src/cogs/finance.py
+++ src/cogs/finance.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+import logging
 import random
 import time
 from datetime import datetime, timedelta
@@ -9,8 +10,10 @@ from classes.bot import Bot
 
 
 class Finance(commands.Cog):
+
     def __init__(self, bot: Bot):
         self.bot = bot
+        self.logger = logging.getLogger("discord.bot.Finance")
 
     @commands.command(
         name="내자산",
@@ -18,11 +21,9 @@ class Finance(commands.Cog):
         description="내 자산, 타인의 자산을 확인합니다."
     )
     async def asset_info(self, ctx: commands.Context, other_user: discord.User = None):
-        self.bot.logger.info(f"{ctx.author}({ctx.author.id}) -> {ctx.message.content}")
-        if other_user == ctx.author:
-            other_user = None
+        self.logger.info(f"{ctx.author}({ctx.author.id}) -> {ctx.message.content}")
 
-        if other_user is None:
+        if other_user == ctx.author or other_user is None:
             user = ctx.author
             user_info = self.bot.database.get_user_info(user.id)
             if not await user_info.is_valid_user():
@@ -40,8 +41,8 @@ class Finance(commands.Cog):
             color=0x00ff00
         )
         embed.add_field(name="자산", value=f"{await user_info.get_money():,}원")
-        
         await ctx.send(embed=embed)
+
 
     @commands.command(
         name="출석체크",
@@ -49,7 +50,7 @@ class Finance(commands.Cog):
         description="출석체크를 합니다."
     )
     async def attendance(self, ctx: commands.Context):
-        self.bot.logger.info(f"{ctx.author}({ctx.author.id}) -> {ctx.message.content}")
+        self.logger.info(f"{ctx.author}({ctx.author.id}) -> {ctx.message.content}")
         user_info = self.bot.database.get_user_info(ctx.author.id)
 
         if not await user_info.is_valid_user(): # 사용자 등록 여부 확인
@@ -70,7 +71,6 @@ class Finance(commands.Cog):
 
         else:
             await ctx.send("출석체크는 하루에 한 번만 가능합니다.")
-            return
 
 
     @commands.command(
@@ -78,34 +78,30 @@ class Finance(commands.Cog):
         description="다른 사람에게 돈을 보냅니다."
     )
     async def send_money(self, ctx: commands.Context, other_user: discord.User, money: int):
-        # db에 유저가 존재하는지 확인
-        # 돈이 부족하지 않는지
-        # 돈이 int타입인지 -> isinstance 사용
         user_info = self.bot.database.get_user_info(ctx.author.id)
         other_user_info = self.bot.database.get_user_info(other_user.id)
 
-        if await user_info.is_valid_user(): # 사용자 등록 여부 확인
-            if other_user.id == ctx.author.id:
-                await ctx.send("자기 자신에게 돈을 송금할 수 없습니다.")
-                return
-            
-            if await other_user_info.is_valid_user(): # 사용자 등록 여부 확인
-                if money <= 0:
-                    await ctx.send("돈은 1원 이상이어야 합니다.")
-                    return
-
-                user_money = await user_info.get_money() # 돈 정보를 가져옴
-                if money <= user_money: # 송금이 가능하면
-                    await user_info.set_money(user_money - money) # 돈 차감
-                    await other_user_info.add_money(money) # 돈 추가
-                    await ctx.send(f"{other_user.display_name}님에게 {money:,}원을 송금했습니다.")
-
-                else:
-                    await ctx.send("돈이 부족합니다.")
-            else:
-                await ctx.send(f"{other_user.display_name}님은 등록되어 있지 않은 유저입니다.")
-        else:
+        if not await user_info.is_valid_user(): # 사용자 등록 여부 확인
             await ctx.send("사용자 등록을 먼저 해 주세요.")
+            return
+        elif not await other_user_info.is_valid_user(): # 사용자 등록 여부 확인
+            await ctx.send(f"{other_user.display_name}님은 등록되어 있지 않은 유저입니다.")
+            return
+        elif other_user.id == ctx.author.id: # 자기 자신에게 돈을 송금하는 경우 예외처리
+            await ctx.send("자기 자신에게 돈을 송금할 수 없습니다.")
+            return
+        elif money <= 0:
+            await ctx.send("송금 금액은 1원 이상이어야 합니다.")
+            return
+
+        user_money = await user_info.get_money() # 돈 정보를 가져옴
+        if money > user_money: # 송금이 불가능하면
+            await ctx.send("돈이 부족합니다.")
+            return
+
+        await user_info.set_money(user_money - money)
+        await other_user_info.add_money(money)
+        await ctx.send(f"{other_user.display_name}님에게 {money:,}원을 송금했습니다.")
 
     @send_money.error
     async def send_money_error(self, ctx: commands.Context, error):
@@ -118,6 +114,7 @@ class Finance(commands.Cog):
         else:
             await ctx.send("오류가 발생했습니다.")
             raise error
+
 
 async def setup(bot: Bot): # setup 함수로 명령어 추가
     await bot.add_cog(Finance(bot))
