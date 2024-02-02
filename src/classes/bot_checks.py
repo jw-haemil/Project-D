cf70ec9ffa21fe40a47ef993a60cb4e9e54343a6
gudtldn <gudtldn1234@gmail.com>
2024-02-02 20:22:01 +0900

    help 명령어 사용시 체크 안하게끔 수정

--- src/classes/bot_checks.py
+++ src/classes/bot_checks.py
@@ -15,6 +15,9 @@ class Checks():
     def is_registered():
         """DB에 사용자가 등록되어있는지 확인"""
         async def predicate(ctx: commands.Context):
+            if ctx.invoked_with == "help": # help 명령어 실행시 체크 안함.
+                return False
+
             logger.debug(f"Checking if {ctx.author.id} is registered")
             info = ctx.bot.database.get_user_info(ctx.author.id)
             if await info.is_valid_user():
