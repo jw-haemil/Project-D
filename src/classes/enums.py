04a1a979230f5d2b6d9480bdd72590f8f2915820
gudtldn <gudtldn1234@gmail.com>
2024-03-11 19:20:05 +0900

    EnumClass에 E접두사 추가 및 EItemTypes추가

--- src/classes/enums.py
+++ src/classes/enums.py
@@ -2,13 +2,13 @@ import discord
 from enum import Enum
 
 
-class UserInfoColumns(Enum):
+class EUserInfoColumns(Enum):
     ID = 0
     MONEY = 1
     CHECK_TIME = 2
 
 
-class FishInfoColumns(Enum):
+class EFishInfoColumns(Enum):
     ID = 0
     NAME = 1
     RATING = 2
@@ -19,7 +19,7 @@ class FishInfoColumns(Enum):
     DESCRIPTION = 7
 
 
-class FishRating(Enum):
+class EFishRating(Enum):
     COMMON = 0 # 일반
     UNCOMMON = 1 # 고급
     RARE = 2 # 희귀
@@ -28,19 +28,25 @@ class FishRating(Enum):
     MYTHIC = 5 # 신화
 
 fish_embed_color = {
-    FishRating.COMMON: discord.Color.light_grey(),
-    FishRating.UNCOMMON: discord.Color.green(),
-    FishRating.RARE: discord.Color.blue(),
-    FishRating.EPIC: discord.Color.purple(),
-    FishRating.LEGENDARY: discord.Color.gold(),
-    FishRating.MYTHIC: discord.Color.red()
+    EFishRating.COMMON: discord.Color.light_grey(),
+    EFishRating.UNCOMMON: discord.Color.green(),
+    EFishRating.RARE: discord.Color.blue(),
+    EFishRating.EPIC: discord.Color.purple(),
+    EFishRating.LEGENDARY: discord.Color.gold(),
+    EFishRating.MYTHIC: discord.Color.red()
 }
 
 fish_kor_name = {
-    FishRating.COMMON: "일반",
-    FishRating.UNCOMMON: "고급",
-    FishRating.RARE: "희귀",
-    FishRating.EPIC: "에픽",
-    FishRating.LEGENDARY: "전설",
-    FishRating.MYTHIC: "신화"
+    EFishRating.COMMON: "일반",
+    EFishRating.UNCOMMON: "고급",
+    EFishRating.RARE: "희귀",
+    EFishRating.EPIC: "에픽",
+    EFishRating.LEGENDARY: "전설",
+    EFishRating.MYTHIC: "신화"
 }
+
+
+class EItemTypes(Enum):
+    FISH = "fish"
+    ITEM = "item"
+    GACHA = "gacha"
